7d3b8e095032ea494e639553e233cdbc198b0139
elyash14 <elyas_mosayebi@yahoo.com>
2022-05-13 13:28:24 +0430

    feat: add login api

---
+++ server/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+// const books = require('./books');
+const users = require("./data/user.json");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.post("/auth/login", (req, res) => {
+  const { email, password } = req.body;
+
+  setTimeout(() => {
+    // check email and password exist in request
+    if (!email || !password) {
+      const validationErrors = {
+        hasError: true,
+        message: "Validation Error",
+        errors: {
+          email: !email ? "Email is required" : null,
+          password: !password ? "Password is required" : null,
+        },
+      };
+      res.status(400).send(validationErrors);
+    } else {
+      // check user with this credentials exists in users.json
+      const user = users.find(
+        (u) => u.email.toLowerCase() === email.toLowerCase()
+      );
+      if (!user) {
+        res.status(404).send({
+          hasError: true,
+          message: "User by this email not found",
+        });
+      }
+      if (user) {
+        if (user.password === password) {
+          const { password, ...rest } = user;
+          res.status(200).send({
+            hasError: false,
+            message: "Successful",
+            payload: rest,
+          });
+        } else {
+          res.status(409).send({
+            hasError: true,
+            message: "Password is wrong",
+          });
+        }
+      }
+    }
+  }, 500);
+});
+
+// app.get("/books",(req,res)=>{
+//     setTimeout(()=>{
+//         res.status(200).send({
+//             hasError:false,
+//             payload:books
+//         })
+//     },600)
+// })
+
+app.listen(9000);
